112b29bc918518c411e1ce3f499d6a4a38e2dacc
bastidio <101216960+bastidio@users.noreply.github.com>
2025-10-30 07:51:05 -0300

    Update script.js

--- proyecto web/script.js
+++ proyecto web/script.js
@@ -161,4 +161,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+
+});
+
+
+const menuToggle = document.getElementById("menu-toggle");
+const mainNav = document.getElementById("main-nav");
+  menuToggle.addEventListener("click", () => {
+    mainNav.classList.toggle("active");
 });
